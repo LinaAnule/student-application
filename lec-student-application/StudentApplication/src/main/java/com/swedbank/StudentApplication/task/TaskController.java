29068cad57d72af766bbb485f1f47e7cd520f67a
Lina <lina.anule@gmail.com>
2024-03-18 12:36:32 +0200

    new directory added with other version of student-application

---
+++ lec-student-application/StudentApplication/src/main/java/com/swedbank/StudentApplication/task/TaskController.java
@@ -0,0 +1,35 @@
+package com.swedbank.StudentApplication.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api/tasks")
+public class TaskController {
+    private TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PutMapping("/{id}")
+    public void updateTask(@RequestBody Task task, @PathVariable long id) {
+        taskService.update(task, id);
+    }
+
+    @GetMapping
+    public List<Task> getTasks() {
+        return taskService.findAll();
+    }
+
+    @PostMapping
+    public void createTask(@RequestBody Task task) {
+        taskService.save(task);
+    }
+
+}
